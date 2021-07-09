6ed1e4ec77b4adb9dee36203411eb7024ee3619f
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-07-09 22:30:20 +0200

    1.0.0

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function deviceMemory(req, res,next){
-req.deviceMemory = req.headers['Device-Memory'] || -1
+req.deviceMemory = req.headers['Device-Memory']
+if(req.deviceMemory==undefined){
+    req.deviceMemory=-1
+}
 next()
 }
 module.exports = deviceMemory
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Device Memory
+![npm](https://img.shields.io/npm/v/device-memory?style=flat-square)
+### A simple Express middleware to get the client device memory.
