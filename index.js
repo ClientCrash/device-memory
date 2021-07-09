fd9dd90366e262a3b3f83749bcaaed5b999b76c1
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-07-09 22:36:53 +0200

    1.0.1

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function deviceMemory(req, res,next){
 req.deviceMemory = req.headers['Device-Memory']
-if(req.deviceMemory==undefined){
-    req.deviceMemory=-1
-}
+if(req.deviceMemory==undefined){req.deviceMemory=-1}
+if(req.deviceMemory>8){req.deviceMemory=8}
 next()
 }
 module.exports = deviceMemory
